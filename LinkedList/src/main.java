0f9dc2a8ee1aa99c1f4c3ee18a00d783ee84506c
Leon Lu <ltlu1993@gmail.com>
2022-05-11 10:35:43 -0400

    Moved LinkedList.java to submission folder

--- LinkedList/src/main.java
+++ LinkedList/src/main.java
@@ -9,26 +9,46 @@ public class main
 		test.add(153);
 		test.add(33);
 		
+		System.out.println(test.toString());
+		System.out.println("Size: " + test.size());
 		
 		// First item in list
 		int firstNum = test.getFirst();
 		int firstNumIndex = test.indexOf(firstNum);
 		System.out.println("Number " + firstNum + " found at index " + firstNumIndex);
 		
+		System.out.println("Number " + test.get(1) + " found at index " + 1);
+		
 		test.removeFirst();
+		
+		System.out.println(test.toString());
+		System.out.println("Size: " + test.size());
 		
 		int firstNum2 = test.getFirst();
 		int firstNumIndex2 = test.indexOf(firstNum2);
 		System.out.println("Number " + firstNum2 + " found at index " + firstNumIndex2);
 		
+		test.removeLast();
+		
+		System.out.println(test.toString());
+		System.out.println("Size: " + test.size());
+		
+		test.remove();
 		
 		
 		test.add(415);
 		test.add(064);
 		test.add(117);
 		
-		test.remove(064);
-		System.out.println("Number " + test.get(2) + " found at index " + 2);
-	}
+		System.out.println(test.toString());
+		System.out.println("Size: " + test.size());
+		
+		
+		test.remove(test.indexOf(064));
+		
+		
+		System.out.println(test.toString());
+		System.out.println("Size: " + test.size());
+	};
 }
 
